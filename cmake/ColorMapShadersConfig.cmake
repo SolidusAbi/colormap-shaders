706ba82e77f8adb7aebd95cbc1313637beb86c75
Abian Hernandez <abianhernandez@outlook.es>
2019-10-23 12:15:27 +0100

    Status messages...

--- cmake/ColorMapShadersConfig.cmake
+++ cmake/ColorMapShadersConfig.cmake
@@ -1,6 +1,13 @@
 # ColorMapShadersConfig.cmake - package configuration file
 
 get_filename_component(SELF_DIR ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
-include(${SELF_DIR}/ColorMapShaders.cmake)
+if (EXISTS ${SELF_DIR}/ColorMapShaders.cmake)
+    include(${SELF_DIR}/ColorMapShaders.cmake)
 
-get_target_property(COLORMAPSHADERS_INCLUDE_DIR ColorMapShaders INTERFACE_INCLUDE_DIRECTORIES)
+    get_target_property(COLORMAPSHADERS_INCLUDE_DIR ColorMapShaders INTERFACE_INCLUDE_DIRECTORIES)
+    message(STATUS "Found Colormap-shaders:")
+    message(STATUS " - Includes: ${COLORMAPSHADERS_INCLUDE_DIR}")
+
+else()
+    message(STATUS "Not Found Colormap-shaders:")
+endif()
